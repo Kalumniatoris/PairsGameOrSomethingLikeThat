9263617719073c999de99698a3b62ad41b1578be
Kalumniatoris <Kalumniatoris@gmail.com>
2021-03-09 20:02:09 +0100

    Shows current selected symbol and (which is probably more important) is somehow playable

--- pairsgame.js
+++ pairsgame.js
@@ -1,101 +1,164 @@
-var colors;
-var bcolors;
+var game = {
+  colorsCount: 12,
+  shapesCount: 4,
+  size: 3,
+  colors: [],
+  bcolors: [],
+  lSymbols: [],
+  currentSymbol:-1
 
-var lSymbols;
-const can=600;
-var shapes=4;
-var colors=12;
-var size=7;
+};
+
+const can = 600;
 var siz;
 
 var canvas;
+var canC;
+var ccs = 100;
 
-function setup() {
-  shapes=4;
-  
-  
-  
-  console.log("setup");
-  print("setup");
-canvas=createCanvas(can,can);
-canvas.parent("gameBoard");
+const bc = (p) =>{
 
-generateBoard(shapes,colors,size);
+p.setup = function() {
+  p.createCanvas(can, can);
+ // canvas.parent("gameBoard");
+  p.background([123,55,0]);
+  generateBoard(game.shapesCount, game.colorsCount, game.size);
+};
+p.draw=function() {
+
+  p.drawSymbols();
+  helper(p,false);
+
+
+};
+
+p.mouseClicked = function(){
+  console.log("clicked");
+  
+  
+   let vcount = size * size;
+  var svc = Math.sqrt(vcount);
+  let qx = Math.floor((p.mouseX) / (siz * 2));
+  let qy = Math.floor((p.mouseY) / (siz * 2));
+
+  qx =  p.min(qx, svc);
+  qx =  p.max(0, qx);
+
+  qy = p.min(qy, svc - 1);
+  qy =  p.max(0, qy);
+  
+   var t = qy * svc + qx;
+    t =  p.min(vcount - 1, t);
+    t =  p.max(0, t);
+  
+   console.log("t: "+t);
+      if(game.currentSymbol==-1){
+        console.log("selecting: ");
+    game.currentSymbol=t;
+    }
+    else if(game.currentSymbol==t){
+      console.log("deselecting");
+     game.currentSymbol=-1; 
+    }
+    else{
+    if(game.lSymbols[t].compareTo(game.lSymbols[game.currentSymbol])){
+      game.lSymbols[t].disabled=true;
+      game.lSymbols[game.currentSymbol].disabled=true;
+      console.log("pair");
+      game.currentSymbol=-1;
+    }
+    }
+    
+};
+
+
+
+p.drawSymbols=function() {
+  var svc = size;
+   p.background(0);
+  game.lSymbols.forEach(
+    (x, n) => {
+      x.Sdraw((n % svc) * siz * 2 + siz, Math.floor(n / svc) * siz * 2 + siz, siz,this);
+    }
+
+  );
+};
+
+
+
+
+
+};
+
+let boardCan=new p5(bc,'gameBoard');
+
+//////
+function generateBoard(shapes, colors, sizesq) {
+  
+  size = sizesq;
+  console.log("Generating board " + [shapes, colors, size]);
+  let vcount = size * size;
+  siz = (can / Math.floor(Math.sqrt(vcount))) / 2;
+  generateColors(colors);
+  generateSymbols(vcount, shapes);
+
 }
-var a;
 
-function draw() {
+function generateColors(count) {
+  let colors = [];
+  let bcolors = [];
+  for (n = 0; n < count; n += 1) {
+    colors.push([Math.floor(boardCan.random(255)), Math.floor(Math.sqrt(255)), Math.floor(boardCan.random(255))]);
+    bcolors.push([boardCan.random(255), boardCan.random(255), boardCan.random(255)]);
+  }
 
-     drawSymbols();
-     helper(canvas);
+  game.colors = colors;
+  game.bcolors = bcolors;
+}
+
+function generateSymbols(count, shapes = 4) {
+  let lSymbols = [];
+  for (n = 0; n < count; n += 1) {
+    lSymbols.push(new Shape(Math.floor(boardCan.random(shapes)), Math.floor(boardCan.random(game.colors.length)), Math.floor(boardCan.random(game.bcolors.length))));
+  }
+  game.lSymbols = lSymbols;
+}
+
+
+
+
+function helper(cv,hints) {
+  cv.strokeWeight(1);
+  cv.noStroke();
+  cv.fill(255);
+  let vcount = size * size;
+  var svc = Math.sqrt(vcount);
+  let qx = Math.floor((cv.mouseX) / (siz * 2));
+  let qy = Math.floor((cv.mouseY) / (siz * 2));
+
+  qx =  cv.min(qx, svc);
+  qx =  cv.max(0, qx);
+
+  qy =  cv.min(qy, svc - 1);
+  qy =  cv.max(0, qy);
+  cv.text(qx, cv.mouseX, cv.mouseY);
+  cv.text(qy, cv.mouseX + 15, cv.mouseY);
+  cv.text(qy * svc + qx, cv.mouseX + 15, cv.mouseY + 10);
+  cv.noFill();
+  cv.stroke(255, 255, 255);
+  cv.square(qx * siz * 2, qy * siz * 2, siz * 2);
+
+  
+    var t = qy * svc + qx;
+    t =  cv.min(vcount - 1, t);
+    t =  cv.max(0, t);
     
 
-}
 
-function drawSymbols(){
-  var svc=size;
-  background(0);
-  lSymbols.forEach(
-        (x,n)=>
-      {
-        x.Sdraw((n%svc)*siz*2+siz,floor(n/svc)*siz*2+siz,siz);
-      }
-      
-      );
-}
-function generateBoard(shapes,colors,sizesq){
-  size=sizesq;
- console.log("Generating board "+[shapes,colors,size]);
-  let vcount=size*size;
-  siz=(can/floor(sqrt(vcount)))/2;
-generateColors(colors);
-generateSymbols(vcount,shapes);
+  if (hints) {
 
-}
-
-function generateSymbols(count,shapes=4){
- lSymbols=[];
- for(n=0;n<count;n+=1){
- lSymbols.push( new Shape(floor(random(shapes)),floor(random(colors.length)),floor(random(bcolors.length))));
- } 
-}
-
-
-function generateColors(count){
-colors=[];
-bcolors=[];
-for(n=0;n<count;n+=1){
-colors.push([floor(random(255)),floor(random(255)),floor(random(255))]);
-bcolors.push([random(255),random(255),random(255)]);
-}
-}
-
-function helper(c){
-     strokeWeight(1);
-     noStroke();
-     fill(255);
-     let vcount=size*size;
-        var svc=sqrt(vcount);
-     let qx=floor((mouseX)/(siz*2));
-     let qy=floor((mouseY)/(siz*2));
-     
-     qx=min(qx,svc);
-     qx=max(0,qx);
-     
-       qy=min(qy,svc-1);
-     qy=max(0,qy);
-      text(qx,mouseX,mouseY);
-      text(qy,mouseX+15,mouseY);
-      text(qy*svc+qx,mouseX+15,mouseY+10);
-      noFill();
-      stroke(255,255,255);
-      square(qx*siz*2,qy*siz*2,siz*2);
-      var t=qy*svc+qx;
-      t=min(vcount-1,t);
-      t=max(0,t);
-      
-        lSymbols.forEach((x,n)=>{if(x.compareTo(lSymbols[t])){noStroke();fill(0,150,0);square(n%svc*siz*2,floor(n/svc)*siz*2,5);}});
-      lSymbols.forEach((x,n)=>{if(x.compareTo2(lSymbols[t],2)){noStroke();fill(150,150,0);square(n%svc*siz*2+10,floor(n/svc)*siz*2,5);}});
-      lSymbols.forEach((x,n)=>{if(x.compareTo2(lSymbols[t],3)){noStroke();fill(0,150,150);square(n%svc*siz*2+20,floor(n/svc)*siz*2,5);}});
-
+    game.lSymbols.forEach((x, n) => { if (x.compareTo(game.lSymbols[t])) {  cv.noStroke();  cv.fill(0, 150, 0);  cv.square(n % svc * siz * 2, Math.floor(n / svc) * siz * 2, 5); } });
+    game.lSymbols.forEach((x, n) => { if (x.compareTo2(game.lSymbols[t], 2)) {  cv.noStroke();  cv.fill(150, 150, 0);  cv.square(n % svc * siz * 2 + 10, Math.floor(n / svc) * siz * 2, 5); } });
+    game.lSymbols.forEach((x, n) => { if (x.compareTo2(game.lSymbols[t], 3)) {  cv.noStroke();  cv.fill(0, 150, 150);  cv.square(n % svc * siz * 2 + 20, Math.floor(n / svc) * siz * 2, 5); } });
+  }
 }
--- uis.js
+++ uis.js
@@ -1,10 +1,10 @@
 //aka buttons and stuffs that are not part of canvas
 
-function generate(){
-//console.log("generating");  
-var cshapes=document.getElementById("numshapes").value;
-var csize=document.getElementById("numsize").value;
-var ccolors=document.getElementById("numcolors").value;
-generateBoard(cshapes,ccolors,csize);
+function generate() {
+    //console.log("generating");  
+    var cshapes = document.getElementById("numshapes").value;
+    var csize = document.getElementById("numsize").value;
+    var ccolors = document.getElementById("numcolors").value;
+    generateBoard(cshapes, ccolors, csize);
 
 }
