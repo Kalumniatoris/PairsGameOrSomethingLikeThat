22940147b2cb43fc7cc7ff7e22efc13681341845
Kalumniatoris <Kalumniatoris@gmail.com>
2021-03-09 20:02:39 +0100

    Shows current selected symbol and (which is probably more important) is somehow playable

---
+++ showcurrent.js
@@ -0,0 +1,23 @@
+
+
+const cc= (p)=>{
+
+  p.setup=function(){
+    p.createCanvas(100,100);
+    p.background([210,121,60]);
+  };
+  
+  p.draw=function(){
+    p.background(0);
+    if(game.currentSymbol>0){
+    game.lSymbols[game.currentSymbol].Sdraw(50,50,50,currentCan);
+    }
+    else{
+    p.background(0);
+   
+    }
+  };
+
+};
+
+let currentCan=new p5(cc,'current');
